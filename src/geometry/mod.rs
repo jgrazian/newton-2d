6117ff65f968e54aef4d2a6eac0af562a6170d7e
jgrazian <JOSEPH.GRAZIANO@ROCKWELLCOLLINS.COM>
2021-01-08 16:22:25 -0500

    abdfksdbfk

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,3 +1,6 @@
+use wasm_bindgen::prelude::*;
+use crate::math::Vec2;
+
 pub mod circle;
 pub mod polygon;
 pub mod shape;
@@ -5,3 +8,31 @@ pub mod shape;
 pub use circle::Circle;
 pub use polygon::Polygon;
 pub use shape::Shape;
+
+pub enum Shapes {
+    Circle(Circle),
+    Polygon(Polygon)
+}
+
+impl Shape for Shapes {
+    fn support(&self, dir: &Vec2) -> Vec2 {
+        match self {
+            Shapes::Circle(c) => c.support(dir),
+            Shapes::Polygon(p) => p.support(dir),
+        }
+    }
+
+    fn area(&self) -> f64 {
+        match self {
+            Shapes::Circle(c) => c.area(),
+            Shapes::Polygon(p) => p.area(),
+        }
+    }
+
+    fn center(&self) -> Vec2 {
+        match self {
+            Shapes::Circle(c) => c.center(),
+            Shapes::Polygon(p) => p.center(),
+        }
+    }
+}
--- src/physics.rs
+++ src/physics.rs
@@ -1,21 +1,26 @@
+use wasm_bindgen::prelude::*;
+
 use crate::geometry::*;
 use crate::math::Vec2;
 
-struct World {
-    objects: Vec<RigidBodyTypes>,
+pub struct World {
+    objects: Vec<RigidBody>,
     bvh: Vec<usize>,
 }
 
-struct RigidBody<T: Shape> {
+#[wasm_bindgen]
+pub struct RigidBody {
     mass: f64,
     density: f64,
     inertia: f64,
     velocity: Vec2,
     acceleration: Vec2,
-    shape: T,
+    shape: Shapes,
 }
 
-enum RigidBodyTypes {
-    Circle(RigidBody<Circle>),
-    Polygon(RigidBody<Polygon>),
+#[wasm_bindgen]
+impl RigidBody {
+    pub fn new(mass: f64) -> RigidBody {
+        RigidBody {mass, density: 0.0, inertia: 0.0, velocity: Vec2::new(0.0, 0.0), acceleration: Vec2::new(0.0, 0.0), shape: Shapes::Circle(Circle::new(Vec2::new(0.0, 0.0), 0.0))}
+    }
 }
